e50767df53cc72171283e9ea53740748e3de9ad1
Eknikitina <miss.aside1996@gmail.com>
2023-06-06 20:33:59 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
@@ -3,7 +3,24 @@ package ru.stqa.mfp.sandbox;
 public class MyFirstProgram {
 
   public static void main (String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("User");
+    hello ("Екатерина");
+
+    int l = 4;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+    int a = 4;
+    int b = 5;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
   }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+public static int area (int len) {
+    return len * len;
+}
+public static int area (int a, int b) {
+    return a * b;
+}
 }
