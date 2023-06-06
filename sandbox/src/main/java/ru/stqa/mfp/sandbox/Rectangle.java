a2b8d26683f58612eeb20c02234ab4a987004e94
Eknikitina <miss.aside1996@gmail.com>
2023-06-06 21:50:49 +0300

    Внесены доп сведения в классы квадрата и прямоугольника

---
+++ sandbox/src/main/java/ru/stqa/mfp/sandbox/Rectangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.mfp.sandbox;
+
+public class Rectangle {
+  public int a;
+  public int b;
+
+  public Rectangle (int a, int b) {
+    this.a = a;
+    this.b = b;
+  }
+  public int area () {
+    return this.a * this.b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/mfp/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.mfp.sandbox;
+
+public class Square {
+  public int l;
+
+  public Square(int l) {
+    this.l = l;
+  }
+
+  public int area () {
+      return this.l * this.l;
+  }
+}
