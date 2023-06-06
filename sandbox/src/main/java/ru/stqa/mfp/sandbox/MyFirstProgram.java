27be3a8961e9fd11d292100f3bb6cd607fec54a8
Eknikitina <miss.aside1996@gmail.com>
2023-06-06 21:51:28 +0300

    Дистанция между точками (4 шт)

--- sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
@@ -7,27 +7,21 @@ public class MyFirstProgram {
     hello("User");
     hello ("Екатерина");
 
-    int l = 4;
-    System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
-    int a = 4;
-    int b = 5;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 
-    double c = 3;
-    double d = 5;
-    System.out.println("Площадь прямоугольника со сторонами " + c + " и " + d + " равна " + area( c, d) );
+
+    Square s = new Square(4);
+    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
+
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p = new Point(0, 2, 4, 6);
+    System.out.println("Дистанция между точками равна " + p.distance());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
-public static int area (int len) {
-    return len * len;
-}
-public static int area (int a, int b) {
-    return a * b;
-}
-public static double area (double c, double d) {
-    return c * d;
-}
-}
+  }
+
+
