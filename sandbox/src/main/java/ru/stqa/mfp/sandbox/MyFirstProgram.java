f450b9aea8c9eabbab1776c416bc9d0ce3fa31fd
Eknikitina <miss.aside1996@gmail.com>
2023-06-07 12:22:23 +0300

    Домашнее задание №2 (вторая попытка)

--- sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
@@ -15,13 +15,17 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    Point p = new Point(2, 4, 6, 8);
-    System.out.println("Дистанция между точками равна " + p.distance());
+    Point p1 = new Point(2, 8);
+    Point p2 = new Point (4, 8);
+    System.out.println(distance(p1, p2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
+  public static double distance (Point p1, Point p2) {
+  return  Math.sqrt((p1.p1 - p2.p1) * (p1.p1 - p2.p1) + (p2.p2 - p1.p2) * (p2.p2 - p1.p2));
   }
+}
 
 
