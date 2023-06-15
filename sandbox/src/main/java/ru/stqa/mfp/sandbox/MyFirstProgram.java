6e1ac1d9eb9bbd8d85de8abb0ff9f9b9a4e7ea03
Eknikitina <miss.aside1996@gmail.com>
2023-06-15 19:55:59 +0300

    Формат написания для себя
    Что Паша объяснял

--- sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
@@ -2,30 +2,25 @@ package ru.stqa.mfp.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     hello("world");
     hello("User");
-    hello ("Екатерина");
-
-
+    hello("Екатерина");
 
     Square s = new Square(4);
     System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
     Rectangle r = new Rectangle(4, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+   // System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    System.out.println(String.format("Площадь прямоугольника со сторонами %d и %d = %d", r.a, r.b, r.area()));
 
-    Point p1 = new Point(2, 8);
-    Point p2 = new Point (4, 8);
-    System.out.println(distance(p1, p2));
+    Point p1 = new Point(2, 6);
+    Point p2 = new Point(4, 8);
+    System.out.println(p1.distance(p2));
   }
 
   public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-  }
-  public static double distance (Point point1, Point point2) {
-  return  Math.sqrt((point1.p1 - point2.p1) * (point1.p1 - point2.p1) + (point2.p2 - point1.p2) * (point2.p2 - point1.p2));
+    //System.out.println("Hello, " + somebody + "!");
+    System.out.printf("Hello, %s!%n", somebody);
   }
 }
-
-
--- sandbox/src/main/java/ru/stqa/mfp/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/mfp/sandbox/Point.java
@@ -4,9 +4,12 @@ public class Point {
   public double p1;
   public double p2;
 
-
   public Point(double p1, double p2) {
     this.p1 = p1;
     this.p2 = p2;
-    }
-    }
+  }
+
+  public double distance(Point point) {
+    return Math.sqrt((point.p1 - this.p1) * (point.p1 - this.p1) + (this.p2 - point.p2) * (this.p2 - point.p2));
+  }
+}
