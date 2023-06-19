3bce8cdc186f1b4d96d9b4d3e04f343099c570da
Eknikitina <miss.aside1996@gmail.com>
2023-06-19 23:39:59 +0300

    Домашнее задание №4
    Тест создания нового контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.mfp.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String lastname;
+  private final String mobile;
+  private final String email;
+
+  public ContactData(String name, String lastname, String mobile, String email) {
+    this.name = name;
+    this.lastname = lastname;
+    this.mobile = mobile;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
