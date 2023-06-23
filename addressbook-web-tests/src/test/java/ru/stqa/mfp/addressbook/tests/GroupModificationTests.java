6591c052c72ef9a85a4f71365c2a1e7c935dea16
Eknikitina <miss.aside1996@gmail.com>
2023-06-23 23:09:21 +0300

    Написание теста GroupModification

---
+++ addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.mfp.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.mfp.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("New group5", "test3", "test4"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
