826e2e02b6aee7094500a4923b961dc1c60ed31c
Eknikitina <miss.aside1996@gmail.com>
2023-07-11 19:19:28 +0300

    3.9 Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/appmanager/NavigationHelper.java
@@ -1,22 +1,32 @@
 package ru.stqa.mfp.addressbook.appmanager;
 
+import com.sun.org.apache.xpath.internal.axes.PredicatedNodeTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase {
 
-  public NavigationHelper(WebDriver wd) {
-    super(wd);
-  }
+    public NavigationHelper(WebDriver wd) {
+        super(wd);
+    }
 
-  public void gotoGroupPage() {
-    click(By.linkText("groups"));
-  }
-  public void gotoContactCreation() {
-    click(By.linkText("add new"));
-  }
+    public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
+        click(By.linkText("groups"));
+    }
 
-  public void goToHomePage() {
-    click(By.linkText("home"));
-  }
+    public void gotoContactCreation() {
+        click(By.linkText("add new"));
+    }
+
+    public void goToHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
+        click(By.linkText("home"));
+    }
 }
