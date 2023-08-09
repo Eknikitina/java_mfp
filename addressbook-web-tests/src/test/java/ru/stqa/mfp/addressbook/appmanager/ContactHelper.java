fb1b6bd801a7e5be790d263c7aa073ca3e79fc27
Eknikitina <miss.aside1996@gmail.com>
2023-08-09 19:32:49 +0300

    проверка по ячейкам в методе all

--- addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/appmanager/ContactHelper.java
@@ -100,9 +100,10 @@ public class ContactHelper extends HelperBase {
         contactCache = new Contacts();
         List<WebElement> elements = wd.findElements(By.name("entry"));
         for (WebElement element : elements) {
+            List<WebElement> cells = element.findElements(By.tagName("td"));
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            String lastname = elements.get(1).getText();
-            String name = elements.get(2).getText();
+            String lastname = cells.get(1).getText();
+            String name = cells.get(2).getText();
             ContactData contact = new ContactData().withId(id).withLastname(lastname).withName(name);
             contactCache.add(contact);
         }
