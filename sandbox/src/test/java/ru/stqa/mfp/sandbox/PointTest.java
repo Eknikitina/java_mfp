5db7a2a37c722c9e85bf283b4968907cc93ab7d3
Eknikitina <miss.aside1996@gmail.com>
2023-09-07 20:41:32 +0300

    Попытка ДЗ 2,3

--- sandbox/src/test/java/ru/stqa/mfp/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/mfp/sandbox/PointTest.java
@@ -6,10 +6,16 @@ import org.testng.annotations.Test;
 public class PointTest {
 
   @Test
-  public void testDistance() {
+  public void testDistanceTrue() {
     Point point1 = new Point(2.0, 6.0);
     Point point2 = new Point(4.0, 8.0);
     Assert.assertEquals(point1.distance(point2), 2.8284271247461903);
+  }
+  @Test
+  public void testDistanceFalse() {
+    Point point1 = new Point(2.0, 4.0);
+    Point point2 = new Point(6.0, 8.0);
+    Assert.assertNotEquals(point1.distance(point2), 2.8284271247461903, "Result is invalid!");
   }
 }
 
