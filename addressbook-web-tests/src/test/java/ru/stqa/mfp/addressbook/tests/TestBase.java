7f3222e224e0eb2f462ec0d01c09e292f85dec6f
Eknikitina <miss.aside1996@gmail.com>
2023-09-12 00:28:18 +0300

    Лекция 6.10. Описание конфигурации тестового стенда

--- addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/tests/TestBase.java
@@ -9,7 +9,8 @@ import ru.stqa.mfp.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(Browser.FIREFOX.browserName());
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", Browser.FIREFOX.browserName()));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
