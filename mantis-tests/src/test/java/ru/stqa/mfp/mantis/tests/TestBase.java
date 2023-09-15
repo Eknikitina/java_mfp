dd472749194ea7e204484b9ff1f9f3635b5a9632
Eknikitina <miss.aside1996@gmail.com>
2023-09-15 22:29:52 +0300

    Лекция 8.2. Подготовка проекта для тестирования MantisBT

---
+++ mantis-tests/src/test/java/ru/stqa/mfp/mantis/tests/TestBase.java
@@ -0,0 +1,22 @@
+package ru.stqa.mfp.mantis.tests;
+
+import org.openqa.selenium.remote.Browser;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.mfp.mantis.appmanager.ApplicationManager;
+
+
+public class TestBase {
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", Browser.FIREFOX.browserName()));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+}
