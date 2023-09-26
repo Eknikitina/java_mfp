3bf8945016d29af2746e2a13b084b291df7c6af2
Eknikitina <miss.aside1996@gmail.com>
2023-09-26 22:01:29 +0300

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/appmanager/ApplicationManager.java
@@ -1,18 +1,18 @@
 package ru.stqa.mfp.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Properties;
 
@@ -36,63 +36,68 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         dbHelper = new DbHelper();
-
-        if (browser.equals(Browser.CHROME.browserName())) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(Browser.FIREFOX.browserName())) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(Browser.IE.browserName())) {
-            wd = new InternetExplorerDriver();
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(Browser.CHROME.browserName())) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(Browser.FIREFOX.browserName())) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(Browser.IE.browserName())) {
+                wd = new InternetExplorerDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-        wd.get(properties.getProperty("web.baseURL"));
-        groupHelper = new GroupHelper(wd);
-        contactHelper = new ContactHelper(wd);
-        navigationHelper = new NavigationHelper(wd);
-        sessionHelper = new SessionHelper(wd);
-        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
-    }
+            wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+            wd.get(properties.getProperty("web.baseURL"));
+            groupHelper = new GroupHelper(wd);
+            contactHelper = new ContactHelper(wd);
+            navigationHelper = new NavigationHelper(wd);
+            sessionHelper = new SessionHelper(wd);
+            sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+        }
 
-    public void logOut() {
-        wd.findElement(By.linkText("Logout")).click();
-    }
+        public void logOut () {
+            wd.findElement(By.linkText("Logout")).click();
+        }
 
-    public void stop() {
-        wd.quit();
-    }
+        public void stop () {
+            wd.quit();
+        }
 
-    public boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
+        public boolean isElementPresent (By by){
+            try {
+                wd.findElement(by);
+                return true;
+            } catch (NoSuchElementException e) {
+                return false;
+            }
+        }
+
+        private boolean isAlertPresent () {
+            try {
+                wd.switchTo().alert();
+                return true;
+            } catch (NoAlertPresentException e) {
+                return false;
+            }
+        }
+
+
+        public DbHelper db () {
+            return dbHelper;
+        }
+        public GroupHelper group () {
+            return groupHelper;
+        }
+
+        public ContactHelper contact () {
+            return contactHelper;
+        }
+
+        public NavigationHelper goTo () {
+            return navigationHelper;
         }
     }
-
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-
-    public DbHelper db() {
-        return dbHelper;
-    }
-    public GroupHelper group() {
-        return groupHelper;
-    }
-
-    public ContactHelper contact() {
-        return contactHelper;
-    }
-
-    public NavigationHelper goTo() {
-        return navigationHelper;
-    }
-}
